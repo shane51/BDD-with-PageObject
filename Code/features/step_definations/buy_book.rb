b2b24f0052a4430f79ed6d5057d8620729f8015c
shane51 <suse51@163.com>
2015-09-01 13:45:15 +0800

    Extract find_element method into a generic method findElementBy(type,value)

--- Code/features/step_definations/buy_book.rb
+++ Code/features/step_definations/buy_book.rb
@@ -8,21 +8,17 @@ When /^I search for "(.*?)"$/ do |keyword|
 end
 
 And /^I open the first book$/ do
-  element = @driver.find_element(:xpath =>"//div[@id=\"result_0\"]//span[@class=\"a-link-normal\"]")
-  element.click
+  openFirstItemInSearchResult
 end
 
 And /^I add the first book to shopping cart$/ do
-  @bookTitle = @driver.find_element(:id => "productTitle").text
-  element = @driver.find_element :name => "submit.add-to-cart"
-  element.click
+  getCurrentItemTitle
+  addToShoppingCart
 end
 
 Then /^I should see the book in my shopping cart$/ do
-  confirm = @driver.find_element(:id => "confirm-text")
-  confirm.text.include?("1 item added to Cart")
-  book = @driver.find_element(:xpath => "//div[@class=\"a-row-a-size-base word-break\"]/a")
-  book.attribute("title").should == @bookTitle
+  verifyItemAddedToShoppingCart
+  verifyItemAddedShownInShoppingCart
 end
 
 def openSite(site)
@@ -30,9 +26,58 @@ def openSite(site)
 end
 
 def searchKeyWord(keyword)
-  searchKeyWord = @driver.find_element(:id => "twotabsearchtextbox")
+  searchKeyWord = findElementBy("id", "twotabsearchtextbox")
   searchKeyWord.clear
   searchKeyWord.send_keys keyword
-  element = @driver.find_element :class => "nav-input"
+  element = findElementBy("class", "nav-input")
   element.click
+end
+
+def openFirstItemInSearchResult
+  element = findElementBy("xpath","//div[@id=\"result_0\"]//span[@class=\"a-link-normal\"]")
+  element.click
+end
+
+def getCurrentItemTitle
+    @bookTitle = findElementBy("id","productTitle").text
+end
+
+def addToShoppingCart
+  element = findElementBy("name","submit.add-to-cart")
+  element.click
+end
+
+def verifyItemAddedToShoppingCart
+  confirm = findElementBy("id", "confirm-text")
+  confirm.text.include?("1 item added to Cart")
+end
+
+def verifyItemAddedShownInShoppingCart
+  book = findElementBy("xpath","//div[@class=\"a-row-a-size-base word-break\"]/a")
+  book.attribute("title").should == @bookTitle
+end
+
+def findElementBy(type, value)
+  case type
+  when "class"
+    @driver.find_element(:class => value)
+  when "id"
+    element = @driver.find_element(:id => value)
+  when "name"
+    element = @driver.find_element(:name => value)
+  when "css"
+    element = @driver.find_element(:css => value)
+  when "id"
+    element = @driver.find_element(:id => value)
+  when "link"
+    element = @driver.find_element(:link => value)
+  when "partial_link"
+    element = @driver.find_element(:partial_link => value)
+  when "tag"
+    element = @driver.find_element(:link => value)
+  when "xpath"
+    element = @driver.find_element(:xpath => value)
+  else
+    puts "Unknown type"
+  end
 end
